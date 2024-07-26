1ab769b434d6c71ff0efe28c144fe8d419cbc98f
priyanshivmehta <pvmehta112@gmail.com>
2024-07-26 15:57:19 +0530

    updated home and style

--- script.js
+++ script.js
@@ -19,6 +19,70 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  let lastScrollTop = 0;
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", function () {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop) {
+          navbar.classList.add("hidden");
+      } else {
+          navbar.classList.remove("hidden");
+      }
+      lastScrollTop = scrollTop;
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeInElements = document.querySelectorAll('.fade-in');
+
+  const observerOptions = {
+      threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              observer.unobserve(entry.target);
+          }
+      });
+  }, observerOptions);
+
+  fadeInElements.forEach(element => {
+      observer.observe(element);
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeInElements = document.querySelectorAll('.fade-in-word');
+
+  const observerOptions = {
+      threshold: 0.1
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              observer.unobserve(entry.target);
+          }
+      });
+  }, observerOptions);
+
+  fadeInElements.forEach(element => {
+      observer.observe(element);
+  });
+});
+
+
+
+
+
+
 // QUIZ
 const questions=[
   {
@@ -197,6 +261,11 @@ nextbutton.addEventListener("click", ()=>{
 })
 
 StartQuiz();
+
+
+
+
+
 
 
 
